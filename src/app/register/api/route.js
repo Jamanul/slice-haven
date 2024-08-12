fc2a00ac49ac4bcbcade35254e5c61406c94f786
Jamanul <jk.skb.25@gmail.com>
2024-08-12 12:23:51 +0600

    api route and register a user done

--- src/app/register/api/route.js
+++ src/app/register/api/route.js
@@ -5,12 +5,15 @@ export const POST = async (request) => {
     const db = await connectDb()
     const userCollection = db.collection('users')
     try {
-        const user = request.json()
+        const user = await request.json()
+        console.log(user)
         const exist = await userCollection.findOne({email: user.email})
+        console.log(exist)
         if(exist){
             return NextResponse.json({message: "user Already exist"})
         }
         const res = await userCollection.insertOne(user)
+        console.log(res)
         return NextResponse.json({message: "user added"},{status: 200})
     } catch (error) {
         console.log(error)
--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,44 +1,97 @@
+"use client"
 import React from "react";
 import bg from "../../../public/pizzaDoodle.jpg";
 import Image from "next/image";
 
 const registerPage = () => {
+  const handleRegister =async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const photoUrl = form.photoUrl.value;
+    const password = form.password.value;
+    const email = form.email.value;
+    const user = {
+      name,
+      photoUrl,
+      password,
+      email,
+    };
+    console.log(user)
+    const res = await fetch('http://localhost:3000/register/api',{
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify(user)
+    })
+    console.log(res)
+  };
   return (
     <div className="h-screen">
-      <Image src={bg} alt="pizza" className="-z-10 absolute" layout="fill"  objectFit="cover" />
+      <Image
+        src={bg}
+        alt="pizza"
+        className="-z-10 absolute"
+        layout="fill"
+        objectFit="cover"
+      />
       <div className="relative flex items-center justify-center h-screen">
-      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-      <form className="card-body">
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Name</span>
-          </label>
-          <input type="text" placeholder="Name" className="input input-bordered" required />
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <form onSubmit={handleRegister} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Name"
+                name="name"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Photo Url</span>
+              </label>
+              <input
+                type="text"
+                name="photoUrl"
+                placeholder="Photo URl"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                name="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                name="password"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn bg-primary text-white">Register</button>
+            </div>
+          </form>
         </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Photo Url</span>
-          </label>
-          <input type="text" placeholder="Photo URl" className="input input-bordered" required />
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
-          
-        </div>
-        <div className="form-control mt-6">
-          <button className="btn bg-primary text-white">Register</button>
-        </div>
-      </form>
-    </div>
       </div>
     </div>
   );
