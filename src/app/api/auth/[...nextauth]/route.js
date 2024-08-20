54315cd939f89365a0bb7dea09152b45eb1ed137
Jamanul <jk.skb.25@gmail.com>
2024-08-20 22:02:06 +0600

    added photourl to images

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -35,7 +35,31 @@ const handler = NextAuth({
             }
         }),
       ],
-    callbacks :{}
+    callbacks :{
+        async jwt({ token, account, user }) {
+            // Persist the OAuth access_token and or the user id to the token right after signin
+            // console.log('account',account)
+            // console.log('user',user)
+            const db =await connectDb()
+            const userCollection = db.collection('users')
+            const currentUser = await userCollection.findOne({email:token.email})
+            console.log(currentUser)
+              if(currentUser){
+                token.photUrl = currentUser?.photoUrl
+              }
+            
+            return token
+          },
+        async session({ session, token, user }) {
+            // Send properties to the client, like an access_token and user id from a provider.
+            //console.log('token',token)
+            //console.log(session.user.photoURL)
+            
+            session.user.image = token?.photUrl
+            console.log(session)
+            return session
+          }
+    }
 })
 
 
