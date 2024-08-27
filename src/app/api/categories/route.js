82e4bf69a65e16f41ff52bfa5c2fc6663af419bd
Jamanul <jk.skb.25@gmail.com>
2024-08-27 16:22:47 +0600

    getting category api and api intregation done

--- src/app/api/categories/route.js
+++ src/app/api/categories/route.js
@@ -5,9 +5,21 @@ export const POST = async(request)=>{
     const categoriesCollection = db.collection('categories')
     try {
         const category =await request.json()
+        //console.log(category)
         const result = await categoriesCollection.insertOne(category)
         return Response.json(result)
     } catch (error) {
         console.log(error)
     }
+}
+
+export const GET =async()=>{
+    const db = await connectDb()
+    const categoriesCollection =await db.collection('categories')
+    try {
+        const result = await categoriesCollection.find().toArray()
+        return Response.json(result)
+    } catch (error) {
+        console.log(error)
+    } 
 }
--- src/app/categories/page.jsx
+++ src/app/categories/page.jsx
@@ -1,21 +1,47 @@
 "use client"
 import UserTabs from '@/components/layout/UserTabs';
 import { usePathname } from 'next/navigation';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const page = () => {
     const [isAdmin,setIsAdmin]=useState(true)
+    const [category,setCategory]=useState([])
     const pathName =usePathname()
+    useEffect(()=>{
+        getCategory()
+    },[])
+    const getCategory =async()=>{
+        const res = await fetch('http://localhost:3000/api/categories',{
+            method: "GET",
+            headers:{
+                "content-type":"application/json"
+            },
+        })
+        const data = await res.json()
+        setCategory(data)
+    }
+    const handleCategories =async(e)=>{
+        e.preventDefault()
+        const name = e.target.category.value
+        const res = await fetch('http://localhost:3000/api/categories',{
+            body: JSON.stringify({category:name}),
+            headers:{
+                "content-type":"application/json"
+            },
+            method: "POST" 
+        })
+        console.log(res)
+    }
     return (
         <div>
          <UserTabs isAdmin={isAdmin} pathname={pathName}/>  
-         <form action="">
+         <form action="" onSubmit={handleCategories}>
             <div className='flex flex-col'>
                  <label >
                 Create A Category
             </label>
             <div className='flex gap-2'>
-            <input type="text"  className='input input-bordered max-w-md grow'/>
+            <input type="text" name='category' className='input input-bordered max-w-md grow'/>
             <input type="submit" value="Create" className='bg-primary btn text-white' />
             </div>
        
